95e56aee27727ac68a35e1262e21791e146cdb04
Thanh Dip <thanhdipin@gmail.com>
2021-11-05 13:03:57 -0700

    Simple pulling data script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # historical-crypto-dataset
+
 Grabs and updates all historical data on all crypto.
+
+## To-do
+
+- [ ] Use async binance calls
+- [ ] Double check parquet timestamps from original data
+- [ ] Use websockets for listening to new klines
--- historical/main.py
+++ historical/main.py
@@ -1,17 +1,77 @@
-import asyncio
 import pandas as pd
+import os.path
+import pyarrow
 from binance import AsyncClient
 from binance.client import Client
+from binance import AsyncClient, BinanceSocketManager
+
+PARQUETFOLDERNAME = "data_archive"
+
+LABELS = [
+    'open_time',
+    'open',
+    'high',
+    'low',
+    'close',
+    'volume',
+    'close_time',
+    'quote_asset_volume',
+    'number_of_trades',
+    'taker_buy_base_asset_volume',
+    'taker_buy_quote_asset_volume'
+]
 
 
-async def main():
-    # initialise the client
-    client = await AsyncClient.create()
-    res = await client.get_exchange_info()
-    print(client.response.headers)
+def all_symbol_pairs(client):
+    "Gets exchange info from Binance and returns a list of symbol pairs as tuples"
+    SYMBOLS = "symbols"
+    BASEASSET = "baseAsset"
+    QUOTEASSET = "quoteAsset"
+    info = client.get_exchange_info()
+    symbol_pairs = [(sym[BASEASSET], sym[QUOTEASSET]) for sym in info[SYMBOLS]]
+    return symbol_pairs
 
-    await client.close_connection()
+
+def saved_parquet(sym_pair):
+    "Gets saved historical parquet data or create new file."
+    filename = f"{sym_pair[0]}-{sym_pair[1]}.parquet"
+    filepath = f"./{PARQUETFOLDERNAME}/{filename}"
+    if os.path.exists(filepath):
+        return pd.read_parquet(filepath)
+    else:
+        df = pd.DataFrame(columns=LABELS)
+        df.to_parquet(filepath, index=False)
+        return df
+
+
+def save_parquet(sym_pair, df, append=""):
+    filename = f"{sym_pair[0]}-{sym_pair[1]}{append}.parquet"
+    print("Saving as: " + filename)
+    filepath = f"./{PARQUETFOLDERNAME}/{filename}"
+    df.to_parquet(filepath, index=False)
+
+
+def main():
+    # initialise the binance api client
+    binance_client = Client()
+    all_pairs = all_symbol_pairs(binance_client)
+
+    for pair in all_pairs:
+        sym = f"{pair[0]}{pair[1]}"
+        saved_df = saved_parquet(pair)
+        print(sym)
+        if saved_df.empty:
+            start = "1 Jan, 1990"
+        else:
+            # Seconds to milliseconds
+            start = str(saved_df["open_time"].max().timestamp() * 1000)
+        for kline in binance_client.get_historical_klines_generator(
+                sym, Client.KLINE_INTERVAL_1MINUTE, start):
+            # Ignore last data from kline
+            saved_df.append(
+                pd.Series(kline[:11], index=saved_df.columns), ignore_index=True)
+        save_parquet(pair, saved_df, "test")
+
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    main()
